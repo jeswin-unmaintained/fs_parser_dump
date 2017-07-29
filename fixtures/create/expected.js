5d64954fe9c56df572da48b4c0777117af5424a7
Jeswin Kumar <jeswinpk@agilehead.com>
2017-07-29 13:00:28 +0530

    Update expected.js

--- fixtures/create/expected.js
+++ fixtures/create/expected.js
@@ -1,50 +1,8 @@
 module.exports = {
   type: "create",
-  itemsNode: {
-    type: "ObjectExpression",
-    properties: [
-      {
-        type: "ObjectProperty",
-        method: false,
-        key: {
-          type: "Identifier",
-          name: "dir"
-        },
-        value: {
-          type: "Identifier",
-          name: "documents"
-        }
-      },
-      {
-        type: "ObjectProperty",
-        method: false,
-        key: {
-          type: "Identifier",
-          name: "filename"
-        },
-        value: {
-          type: "Identifier",
-          name: "report.txt"
-        }
-      }
-      {
-        type: "ObjectProperty",
-        method: false,
-        key: {
-          type: "Identifier",
-          name: "contents"
-        },
-        value: {
-          type: "Identifier",
-          name: "hello, world"
-        }
-      }
-    ]
-  },
-  source: {
-    type: "query",
-    module: "todosFsModule",
-    identifier: "myFs"
-    // ,path: "todos"
-  }
+  dir: "documents",
+  filename: "report.txt",
+  contents: "hello, world",
+  module: "todosFsModule",
+  identifier: "myFs"
 }
